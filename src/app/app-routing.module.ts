e9478e28bc48dc33a78aa9f7444f92daf7c02724
Ramezzorgui <Ramez.Zorgui@esprit.tn>
2024-12-21 11:33:13 +0100

    Final commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TeamComponent } from './Team/team/team.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: 'Show teams',
+    component: TeamComponent},
+    {
+      path: 'Teams/team',
+      component: TeamComponent},
+      {path: 'Team',
+    loadChildren: () =>
+      import('./Team/team/team.component').then((m) => m.TeamComponent)},
+
+  
+  ]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './Components/nav-bar/nav-bar.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { NotFoundComponent } from './Components/not-found/not-found.component';
+import { TeamsComponent } from './Team/teams/teams.component';
+import { TeamComponent } from './Team/team/team.component';
+import { FormTeamComponent } from './Team/form-team/form-team.component';
+import { MyTeamsComponent } from './Team/my-teams/my-teams.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    FooterComponent,
+    NotFoundComponent,
+    TeamsComponent,
+    TeamComponent,
+    FormTeamComponent,
+    MyTeamsComponent,
   ],
   imports: [
     BrowserModule,
